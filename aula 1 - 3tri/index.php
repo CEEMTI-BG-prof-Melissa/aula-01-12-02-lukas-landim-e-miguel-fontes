5985397436174238f73201c1baac1f9c2e3f1129
Lukas R. Landim <lukas.rlandim@aluno.edu.es.gov.br>
2025-09-23 11:42:10 -0300

    Create index.php

---
+++ aula 1 - 3tri/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Aula 01 PHP </title>
+    </head>
+    <body>
+        <?php 
+            echo "OLÁ, MUNDO!!!!!!";
+            echo "LUKAS E MIGUEL F, e papai do céu amém";
+        ?>   
+    </body>
+</html>
